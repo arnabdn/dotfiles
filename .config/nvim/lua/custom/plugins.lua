c142c7bc249a78c685686a8a772f9bac2a5dde48
arnabdn <arnabdebnath095@gmail.com>
2024-06-18 19:26:12 +0600

    added neovim config

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  {
+    "ThePrimeagen/vim-be-good",
+    --uncomment if you get issues
+    lazy = false,
+  },
+    {
+        "frabjous/knap",
+        lazy = false
+    }
+}
+
+return plugins
